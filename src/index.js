a708ae7a4a3646af7df18eaedc6f660fc983efd2
Toni Soler <tonisoler1986@gmail.com>
2023-04-23 23:05:55 +0200

    not finished yet

--- src/index.js
+++ src/index.js
@@ -4,6 +4,29 @@ function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
 
   //... your code goes here
+  const price = product.querySelector(".price span");
+  console.log("price", price)
+
+  const quantity = product.querySelector(".quantity input")
+  console.log("input quantity", quantity)
+
+  const priceValue = price.innerHTML
+  console.log("price value", priceValue)
+
+  const quantityValue = quantity.value
+  console.log("quantity value", quantityValue)
+
+  const result = priceValue * quantityValue
+  console.log("result", result)
+
+  const total = product.querySelector(".subtotal span")
+  console.log("total", total)
+console.log(total.innerHTML)
+
+total.innerHTML = result;
+
+
+
 }
 
 function calculateAll() {
